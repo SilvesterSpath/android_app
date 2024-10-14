51214da8d9f994d229356e25d370aba1e0ffc2a5
SilvesterSpath <spath.sz@gmail.com>
2024-10-14 14:46:33 +0200

    component screen elements

--- src/screens/ComponentScreen.jsx
+++ src/screens/ComponentScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
   textStyle: {
@@ -8,7 +8,25 @@ const styles = StyleSheet.create({
 });
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>First android text</Text>;
+    const greeting = 'Bye there!';
+    const numbers = [123, 234];
+    const strings = ['abc', 'dfg']
+    // but not objects
+    const jsx = <Text>This is a Jsx element</Text>
+
+  return (
+      <View>
+        <Text style={styles.textStyle}>First android text</Text>
+        <Text >Hello there!</Text>
+        <Text >{greeting}</Text>
+        <Text >{numbers}</Text>
+        <Text >{strings}</Text>
+        {jsx}
+      </View>
+)
+
 };
+
+
 
 export default ComponentsScreen;
