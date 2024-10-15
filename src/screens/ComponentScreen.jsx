0e2d41f00b2cedfded7c02e985d01b8b8e2882a8
SilvesterSpath <spath.sz@gmail.com>
2024-10-15 09:18:53 +0200

    styles

--- src/screens/ComponentScreen.jsx
+++ src/screens/ComponentScreen.jsx
@@ -5,28 +5,28 @@ const styles = StyleSheet.create({
   textStyle: {
     fontSize: 30,
   },
+  subHeaderStyle: {
+    fontSize: 20,
+  },
 });
 
 const ComponentsScreen = () => {
-    const greeting = 'Bye there!';
-    const numbers = [123, 234];
-    const strings = ['abc', 'dfg']
-    // but not objects
-    const jsx = <Text>This is a Jsx element</Text>
+  const greeting = 'Bye there!';
+  const numbers = [123, 234];
+  const strings = ['abc', 'dfg'];
+  // but not objects
+  const jsx = <Text>This is a Jsx element</Text>;
 
   return (
-      <View>
-        <Text style={styles.textStyle}>First android text</Text>
-        <Text >Hello there!</Text>
-        <Text >{greeting}</Text>
-        <Text >{numbers}</Text>
-        <Text >{strings}</Text>
-        {jsx}
-      </View>
-)
-
+    <View>
+      <Text style={styles.textStyle}>First android text</Text>
+      <Text>Hello there!</Text>
+      <Text style={{ fontSize: 40 }}>{greeting}</Text>
+      <Text style={styles.subHeaderStyle}>{numbers}</Text>
+      <Text>{strings}</Text>
+      {jsx}
+    </View>
+  );
 };
-
-
 
 export default ComponentsScreen;
