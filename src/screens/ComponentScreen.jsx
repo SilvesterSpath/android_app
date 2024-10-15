31f70e050937ce5aede064285f3b4380f4a4d1bb
SilvesterSpath <spath.sz@gmail.com>
2024-10-15 09:50:13 +0200

    FlatList

--- src/screens/ComponentScreen.jsx
+++ src/screens/ComponentScreen.jsx
@@ -14,15 +14,24 @@ const ComponentsScreen = () => {
   const greeting = 'Bye there!';
   const numbers = [123, 234];
   const strings = ['abc', 'dfg'];
+  const name = 'Silvester';
   // but not objects
   const jsx = <Text>This is a Jsx element</Text>;
+
+  function greetings(name) {
+    return `Hello ${name}`;
+  }
 
   return (
     <View>
       <Text style={styles.textStyle}>First android text</Text>
       <Text>Hello there!</Text>
-      <Text style={{ fontSize: 40 }}>{greeting}</Text>
+      <Text style={{ fontSize: 40 }}>
+        {greeting} {name}{' '}
+      </Text>
       <Text style={styles.subHeaderStyle}>{numbers}</Text>
+      <Text style={styles.subHeaderStyle}>{greetings(name)}</Text>
+
       <Text>{strings}</Text>
       {jsx}
     </View>
