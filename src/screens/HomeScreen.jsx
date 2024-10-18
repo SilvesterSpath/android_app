2d4c9d4e8751e907badfe94677e25e3f917c650b
SilvesterSpath <spath.sz@gmail.com>
2024-10-18 06:28:08 +0200

    Image screen init

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -8,6 +8,8 @@ const HomeScreen = ({ navigation }) => {
     if (e.target.textContent === 'Go to components demo') {
       console.log('Component clicked');
       navigation.navigate('Components');
+    } else if (e.target.textContent === 'Go to image element') {
+      navigation.navigate('Image');
     } else {
       navigation.navigate('List');
       console.log('List clicked');
@@ -22,9 +24,11 @@ const HomeScreen = ({ navigation }) => {
       <Button onPress={clickHandler} title='Go to components demo' />
       <p></p>
       <Button onPress={clickHandler} title='Go to list element' />
-      <TouchableOpacity onPress={clickHandler}>
+      <p></p>
+      <Button onPress={clickHandler} title='Go to image element' />
+      {/*<TouchableOpacity onPress={clickHandler}>
         <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   );
 };
---
+++ src/screens/ImageScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+
+const ImageScreen = () => {
+  return <div>ImageScreen</div>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default ImageScreen;
