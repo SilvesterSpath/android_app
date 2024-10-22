7b706667587809412c55e2dc608db56e467fb41e
SilvesterSpath <spath.sz@gmail.com>
2024-10-22 10:18:48 +0200

    imageDetail title props

--- src/components/ImageDetail.jsx
+++ src/components/ImageDetail.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const ImageDetail = () => {
-  return <div>ImageDetail</div>;
+const ImageDetail = ({ title }) => {
+  return <div>ImageDetail: {title}</div>;
 };
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
 
 export default ImageDetail;
--- src/screens/ImageScreen.jsx
+++ src/screens/ImageScreen.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
-  return <div>ImageScreen</div>;
+  return (
+    <div>
+      ImageScreen
+      <ImageDetail title='Forest' />
+      <ImageDetail title='Mountains' />
+      <ImageDetail title='See' />
+    </div>
+  );
 };
 
 const styles = StyleSheet.create({
