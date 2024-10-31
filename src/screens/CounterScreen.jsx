b95f5b453a8a40b2ce3665b58e43305c76167a7e
SilvesterSpath <spath.sz@gmail.com>
2024-10-31 06:16:53 +0100

    counter screen finish

--- src/screens/CounterScreen.jsx
+++ src/screens/CounterScreen.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, StyleSheet, View, Button } from 'react-native';
 
 const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    // setCounter(counter + 1);
+    const newCounter = counter + 1;
+    setCounter(newCounter);
+  };
+
   return (
     <View>
-      <Text>Current Count</Text>
-      CounterScreen
+      <Text>Current Count: {counter}</Text>
+      <Button onPress={increment} title='Increment' />
     </View>
   );
 };
