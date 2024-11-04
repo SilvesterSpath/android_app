b0ba2510a0ee7b21ba33f5fb58e691eb149078e2
SilvesterSpath <spath.sz@gmail.com>
2024-11-04 09:08:47 +0100

    random color box

--- src/screens/ColorScreen.jsx
+++ src/screens/ColorScreen.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
 
 const ColorScreen = () => {
-  return <div>ColorScreen</div>;
+  const [color, setColor] = useState('red');
+  const [left, setLeft] = useState(10);
+  const [top, setTop] = useState(10);
+
+  const randomPosition = () => {
+    const left = Math.floor(Math.random() * 256);
+    const top = Math.floor(Math.random() * 256);
+    setLeft(left);
+    setTop(top);
+  };
+
+  const randomRgb = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    setColor(`rgb(${red}, ${green}, ${blue})`);
+    randomPosition();
+  };
+
+  return (
+    <View>
+      <Button title='Add a Color' onPress={randomRgb} />
+      <View
+        style={{
+          height: 100,
+          width: 100,
+          left: left,
+          top: top,
+          backgroundColor: color,
+        }}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({});
 
 export default ColorScreen;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -4,34 +4,35 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 const HomeScreen = ({ navigation }) => {
   // console.log(props);
 
-  const clickHandler = (e) => {
-    if (e.target.textContent === 'Go to components demo') {
-      navigation.navigate('Components');
-    } else if (e.target.textContent === 'Go to image element') {
-      navigation.navigate('Image');
-    } else if (e.target.textContent === 'Go to list element') {
-      navigation.navigate('List');
-    } else if (e.target.textContent === 'Go to counter element') {
-      navigation.navigate('Counter');
-    } else if (e.target.textContent === 'Go to color element') {
-      navigation.navigate('Color');
-    }
-  };
-
   return (
     <View>
       <Text style={styles.text}>Hi there!</Text>
       <p></p>
-      <Button onPress={clickHandler} title='Go to components demo' />
+      <Button
+        onPress={() => navigation.navigate('Components')}
+        title='Go to components demo'
+      />
       <p></p>
-      <Button onPress={clickHandler} title='Go to list element' />
+      <Button
+        onPress={() => navigation.navigate('List')}
+        title='Go to list element'
+      />
       <p></p>
-      <Button onPress={clickHandler} title='Go to image element' />
+      <Button
+        onPress={() => navigation.navigate('Image')}
+        title='Go to image element'
+      />
       <p></p>
-      <Button onPress={clickHandler} title='Go to counter element' />
+      <Button
+        onPress={() => navigation.navigate('Counter')}
+        title='Go to counter element'
+      />
 
       <p></p>
-      <Button onPress={clickHandler} title='Go to color element' />
+      <Button
+        onPress={() => navigation.navigate('Color')}
+        title='Go to color element'
+      />
       {/*<TouchableOpacity onPress={clickHandler}>
         <Text>Go to List Demo</Text>
       </TouchableOpacity> */}
