6f15ff35a5b7b76bea9d9512c490f24d4df1a853
SilvesterSpath <spath.sz@gmail.com>
2024-11-11 12:27:36 +0100

    increase decrease color

--- src/screens/ColorScreen_3.jsx
+++ src/screens/ColorScreen_3.jsx
@@ -2,54 +2,38 @@ import React, { useState } from 'react';
 import { View, Button, FlatList } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
+const COLOR_INCREMENT = 15;
+
 const ColorScreen_3 = () => {
-  const [colors, setColors] = useState([]);
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
-  const randomRgb = () => {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-    const color = `rgb(${red}, ${green}, ${blue})`;
-
-    return color;
-  };
-
-  console.log(red);
-  console.log(green);
-  console.log(blue);
+  console.log(red, green, blue);
 
   const incrementRed = () => {
-    red < 255 && setRed(red + 1);
+    red < 255 && setRed(red + COLOR_INCREMENT);
   };
   const decrementRed = () => {
-    red > 0 && setRed(red - 1);
+    red > 0 && setRed(red - COLOR_INCREMENT);
   };
 
   const incrementGreen = () => {
-    green < 255 && setGreen(green + 1);
+    green < 255 && setGreen(green + COLOR_INCREMENT);
   };
   const decrementGreen = () => {
-    green > 0 && setGreen(green - 1);
+    green > 0 && setGreen(green - COLOR_INCREMENT);
   };
 
   const incrementBlue = () => {
-    blue < 255 && setBlue(blue + 1);
+    blue < 255 && setBlue(blue + COLOR_INCREMENT);
   };
   const decrementBlue = () => {
-    blue > 0 && setBlue(blue - 1);
+    blue > 0 && setBlue(blue - COLOR_INCREMENT);
   };
 
   return (
     <View>
-      <Button
-        title='Add a Color'
-        onPress={() => {
-          setColors([...colors, randomRgb()]);
-        }}
-      />
       <ColorCounter
         color='Red'
         value={red}
@@ -68,20 +52,12 @@ const ColorScreen_3 = () => {
         increment={incrementBlue}
         decrement={decrementBlue}
       />
+      <p></p>
       <View
         style={{
-          height: 100,
-          width: 100,
+          height: 150,
+          width: 150,
           backgroundColor: `rgb(${red}, ${green}, ${blue})`,
-        }}
-      />
-      <FlatList
-        keyExtractor={(item) => item}
-        data={colors}
-        renderItem={({ item }) => {
-          return (
-            <View style={{ height: 100, width: 100, backgroundColor: item }} />
-          );
         }}
       />
     </View>
